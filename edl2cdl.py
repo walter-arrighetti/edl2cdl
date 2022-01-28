58253ff1b0bc7127a3308b13565efcafdb02c781
Jesse Spielman <jesse.spielman@gmail.com>
2022-01-28 17:34:35 +0000

    Fix "tape" regex

--- edl2cdl.py
+++ edl2cdl.py
@@ -87,7 +87,7 @@ camre = re.compile(r"[*]\s?FROM\sCLIP\sNAME:\s+.*(?P<name>[A-Z][0-9]{3}[_]?C[0-9
 camre0 = re.compile(r"[*]\s?FROM\sCLIP\sNAME:\s+(?P<name>.{63})")
 camre1 = re.compile(r"[*]\s?(?P<name>.*)")
 input_desc, viewing_desc = None, "EDL2CDL script by Walter Arrighetti"
-tapere = re.compile(r"[*]\s?FROM\sCLIP\sNAME:\s+(?P<name>[A-Za-z0-9-_,.]|\s{8,32})")
+tapere = re.compile(r"[*]\s?FROM\sCLIP\sNAME:\s+(?P<name>(\d{3}_\d{4})|[A-Za-z0-9-_,.]|\s{8,32})")
 cdl1re = re.compile(r"[*]\s?ASC[_]SOP\s+[(]\s?(?P<sR>[-]?\d+[.]\d{4,6})\s+(?P<sG>[-]?\d+[.]\d{4,6})\s+(?P<sB>[-]?\d+[.]\d{4,6})\s?[)]\s?[(]\s?(?P<oR>[-]?\d+[.]\d{4,6})\s+(?P<oG>[-]?\d+[.]\d{4,6})\s+(?P<oB>[-]?\d+[.]\d{4,6})\s?[)]\s?[(]\s?(?P<pR>[-]?\d+[.]\d{4,6})\s+(?P<pG>[-]?\d+[.]\d{4,6})\s+(?P<pB>[-]?\d+[.]\d{4,6})\s?[)]\s?")
 cdl2re = re.compile(r"[*]\s?ASC[_]SAT\s+(?P<sat>\d+[.]\d{4,6})")
 
